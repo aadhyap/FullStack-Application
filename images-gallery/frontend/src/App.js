0ad6d94c30b6d7682a3440c4b19efacc0afd3d53
Aadhya Puttur <putturaadhya@gmail.com>
2023-11-24 09:54:29 -0500

    Showing image card on app
    When searching for image, it will appear on site with description and title

--- images-gallery/frontend/src/App.js
+++ images-gallery/frontend/src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+import ImageCard from './components/ImageCard';
 
 const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
@@ -17,7 +18,7 @@ function App() {
     )
       .then((res) => res.json())
       .then((data) => {
-        setImages([data, ...images]);
+        setImages([{ ...data, title: word }, ...images]);
       })
       .catch((err) => {
         console.log(err);
@@ -29,6 +30,7 @@ function App() {
     <div>
       <Header title="Images Gallery" />
       <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit} />
+      {!!images.length && <ImageCard image={images[0]} />}
     </div>
   );
 }
